3beec9bb3db3e90c5b25d1a7f0ce7c5c3721db62
Alexey Boklogov <kashira87@mail.ru>
2021-07-26 22:59:05 +0300

    добавляет секцию профиля без стилей

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Profile from './social-profile/Profile';
+import user from './social-profile/user.json';
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+}
